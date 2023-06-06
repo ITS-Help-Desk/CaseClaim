e0cfe67046d85c4c0f5f13a25f0133f0e5bb0908
Andrew Albizati <aalbizati@sandiego.edu>
2023-06-06 15:28:02 -0700

    Overhauled /leaderboard command

--- bot/cogs/leaderboard_command.py
+++ bot/cogs/leaderboard_command.py
@@ -2,12 +2,12 @@ from discord import app_commands
 from discord.ext import commands
 import discord
 import csv
-import time
 import datetime
-from .. import paginator
+
+from bot.views.leaderboard_view import LeaderboardView
 
 # Use TYPE_CHECKING to avoid circular import from bot
-from typing import TYPE_CHECKING, Optional, Union
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from ..bot import Bot
@@ -30,88 +30,8 @@ class LeaderboardCommand(commands.Cog):
         Args:
             interaction (discord.Interaction): Interaction that the slash command originated from
         """
-        await interaction.response.defer(ephemeral=True) # Wait in case process takes a long time
+        await interaction.response.defer() # Wait in case process takes a long time
 
-        # Count the amount of cases worked on by each user
-        counts = {}
-        first_date = None
-        with open('log.csv', 'r') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                # Find first date
-                if first_date is None:
-                    first_date = row[1]
+        embed = LeaderboardView.create_embed(interaction.created_at, self.bot.embed_color)
 
-                id = int(row[3])
-                if not id in counts.keys():
-                    counts[id] = 0
-                counts[id] += 1
-        
-        row_strs = []
-        counts_sorted_keys = sorted(counts, key=counts.get, reverse=True)
-
-        for i in range(len(counts_sorted_keys)):
-            desc = ''
-            key = counts_sorted_keys[i]
-            desc += f'{i + 1}: '
-            
-            # Get user, replace with ID if absent
-            user: Union[Optional[discord.Member], int]
-            try:
-                user = await interaction.guild.fetch_member(key)
-            except:
-                user = key
-            if user is None:
-                user = key
-            
-            if type(user) == int:
-                desc += f'**{user}**'
-            else:
-                desc += f'**{user.display_name}**'
-            desc += f' ({counts[key]})'
-            row_strs.append(desc)
-
-        titles = f'ITS Help Desk Case Leaderboard'
-        try:
-            start_time = datetime.datetime.strptime(first_date, "%Y-%m-%d %H:%M:%S.%f")  
-        except:
-            start_time = datetime.datetime.now()
-        embeds = self.create_paginator_embeds(row_strs, titles, start_time)
-        await paginator.Simple(ephemeral=True).start(interaction, embeds)
-
- 
-    def create_paginator_embeds(self, data: list[str], title: str, start_time: datetime.datetime) -> list[discord.Embed]:
-        """Creates a list of embeds that can be used with a paginator.
-
-        Args:
-            data (list[str]): The list of case descriptions.
-            title (str): The title for each of the embeds
-
-        Returns:
-            list[discord.Embed]: A list of embeds for the paginator.
-        """
-        # Create a list of embeds to paginate
-        embeds = []
-        i = 0
-        data_len = len(data)
-
-        # Go through all cases
-        while i < data_len:
-            # Create an embed for every 10 cases
-            new_embed = discord.Embed(title=title)
-            new_embed.colour = self.bot.embed_color
-            new_embed.set_footer(text="Starting from")
-            new_embed.timestamp = start_time
-            description = ''
-            
-            # Add ten (or fewer) cases
-            for j in range(min(10, len(data))):
-                row = data.pop(0)
-                description += row + '\n'
-            
-            new_embed.description = description
-            embeds.append(new_embed)
-            i += 10
-
-        return embeds
-        
+        await interaction.followup.send(embed=embed, view=LeaderboardView(self.bot))
---
+++ bot/views/leaderboard_view.py
@@ -0,0 +1,156 @@
+import csv
+import datetime
+import discord
+import discord.ui as ui
+
+# Use TYPE_CHECKING to avoid circular import from bot
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ..bot import Bot
+
+
+class LeaderboardView(ui.View):
+    def __init__(self, bot: "Bot"):
+        """Creates a leaderboard view for the /leaderboard command
+        to allow users to refresh it.
+
+        Args:
+            bot (Bot): A reference to the original Bot instantiation.
+        """
+        super().__init__(timeout=None)
+        self.bot = bot
+
+	
+    @ui.button(label="Refresh", style=discord.ButtonStyle.primary, custom_id="refresh")
+    async def button_refresh(self, interaction: discord.Interaction, button):
+        await interaction.response.defer(thinking=False) # Acknowledge button press
+        
+        new_embed = LeaderboardView.create_embed(interaction.created_at, self.bot.embed_color)
+
+        await interaction.message.edit(embed=new_embed)
+    
+    
+    @ui.button(label="My Rank", style=discord.ButtonStyle.secondary, custom_id="myrank")
+    async def button_myrank(self, interaction: discord.Interaction, button):
+        pass
+        '''self.case = self.bot.get_case(interaction.message.id)
+
+        data = LeaderboardView.get_data(interaction.created_at)
+
+        mc = data[0][0]
+        sc = data[1][0]
+        
+        mc_sorted_keys = data[0][1]
+        sc_sorted_keys = data[1][1]
+
+
+
+
+        await interaction.response.send_message(embed=embed, ephemeral=True)'''
+    
+
+    @staticmethod
+    def create_embed(interaction_date: datetime.datetime, embed_color: discord.Color) -> discord.Embed:
+        month_ranking, semester_ranking = LeaderboardView.create_rankings(interaction_date)
+
+        # Create embed
+        embed = discord.Embed(title="ITS Case Claim Leaderboard")
+        embed.color = embed_color
+
+        embed.add_field(name=f"{LeaderboardView.month_number_to_name(interaction_date.month)} Ranks", value=month_ranking, inline=True)
+        embed.add_field(name="Semester Ranks", value=semester_ranking, inline=True)
+        embed.set_footer(text="Last Updated")
+        embed.timestamp = datetime.datetime.now()
+
+        return embed
+
+
+    @staticmethod
+    def create_rankings(interaction_date: datetime.datetime) -> tuple[str]:
+        data = LeaderboardView.get_data(interaction_date)
+
+        mc = data[0][0]
+        sc = data[1][0]
+        
+        mc_sorted_keys = data[0][1]
+        sc_sorted_keys = data[1][1]
+        
+        month_ranks = []
+        semester_ranks = []
+
+        # Create month written ranking
+        for i in range(min(4, len(mc_sorted_keys))):
+            month_id = mc_sorted_keys[i]
+            month_ranks.append(f"{i + 1}. <@!{month_id}> ({mc[month_id]})")
+        
+        month_ranking = "\n".join(user for user in month_ranks)
+
+        # Create year written ranking
+        for i in range(min(4, len(sc_sorted_keys))):
+            semester_id = sc_sorted_keys[i]
+            semester_ranks.append(f"{i + 1}. <@!{semester_id}> ({sc[semester_id]})")
+
+        semester_ranking = "\n".join(user for user in semester_ranks)
+
+        return month_ranking, semester_ranking
+
+
+    @staticmethod
+    def get_data(interaction_date: datetime.datetime) -> tuple[tuple[dict[int, int]]]:
+        # Count the amount of cases worked on by each user
+        semester_counts = {}
+        month_counts = {}
+        with open('log.csv', 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                date = datetime.datetime.strptime(row[1], "%Y-%m-%d %H:%M:%S.%f")
+    
+                id = int(row[3])
+
+                # Organize data for month
+                if date.month == interaction_date.month:
+                    if not id in month_counts.keys():
+                        month_counts[id] = 0
+                    month_counts[id] += 1
+
+                # Organize data for semester
+                if date.year == interaction_date.year:
+                    if not id in semester_counts.keys():
+                        semester_counts[id] = 0
+                    semester_counts[id] += 1
+                
+        semester_counts_sorted_keys = sorted(semester_counts, key=semester_counts.get, reverse=True)
+        month_counts_sorted_keys = sorted(month_counts, key=month_counts.get, reverse=True)
+
+        return ((month_counts, month_counts_sorted_keys), (semester_counts, semester_counts_sorted_keys))
+    
+
+    @staticmethod
+    def get_individual_ranking():
+        pass
+
+
+    @staticmethod
+    def month_number_to_name(month_number: int) -> str:
+        """Converts a month number to the actual name.
+        (e.g. 1 -> January)
+
+        Args:
+            month_number (int): The number of the month (from 1 to 12)
+
+        Raises:
+            ValueError: If the number provided is < 1 or > 12
+
+        Returns:
+            str: The full name of the month (e.g. "February")
+        """
+        month_names = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ]
+
+        if 1 <= month_number <= 12:
+            return month_names[month_number - 1]
+        else:
+            raise ValueError("Invalid month number")
