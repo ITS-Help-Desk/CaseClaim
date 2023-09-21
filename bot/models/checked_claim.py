b220e73bc38dee6562612570f4199984e6da56cb
Andrew Albizati <andrew.albizati@gmail.com>
2023-09-20 17:28:58 -0700

    Fixed SQL condition

--- bot/models/checked_claim.py
+++ bot/models/checked_claim.py
@@ -167,7 +167,7 @@ class CheckedClaim(DatabaseItem):
             sql += f" AND claim_time BETWEEN {beginning} AND {ending}"
 
         if status is not None:
-            if status == Status.PINGED:
+            if status == Status.PINGED or status == Status.RESOLVED:
                 sql += f" AND (`status` = '{status}' OR `status` = '{Status.RESOLVED}'"
             else:
                 sql += f" AND `status` = '{status}'"
