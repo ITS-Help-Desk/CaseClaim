23b8decfa95625e5e5228cb60ca770e7197bd415
Yooply <48575403+Yooply@users.noreply.github.com>
2023-11-30 16:20:39 -0800

    Stylize page using bootstrap; Fix cards and padding

--- web/components.py
+++ web/components.py
@@ -16,36 +16,36 @@ def nav_column(active: SidebarOptions) -> str:
     STATISTICS_REL_PATH = "/stats"
     dashboard=f"""
     <li class="nav-item">
-        <a href="{DASHBOARD_REL_PATH}" class="nav-link{" active" if active == SidebarOptions.DASHBOARD else ""}">
+        <a href="{DASHBOARD_REL_PATH}" class="nav-link d-flex align-items-center gap-2{" active" if active == SidebarOptions.DASHBOARD else ""}">
             <span data-feather="server"></span>
-            Dashboard {'<span class="sr-only">(current)</span>' if active == SidebarOptions.DASHBOARD else ""}
+            Dashboard
             </a>
     </li>"""
     
     statistics=f"""
     <li class="nav-item">
-        <a href="{STATISTICS_REL_PATH}" class="nav-link{" active" if active == SidebarOptions.STATISTICS else ""}">
+        <a href="{STATISTICS_REL_PATH}" class="nav-link d-flex align-items-center gap-2{" active" if active == SidebarOptions.STATISTICS else ""}">
             <span data-feather="bar-chart"></span>
-            Statistics {'<span class="sr-only">(current)</span>' if active == SidebarOptions.STATISTICS else ""}
+            Statistics 
             </a>
     </li>"""
     
     nav_column = """
-    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-        <div class="sidebar-sticky">
+    <div class="sidebar col-md-3 col-lg-2 p-0 bg-body-tertiary">
+        <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu">
             <ul class="nav flex-column">"""
     nav_column += dashboard
     nav_column += statistics
     nav_column += """
             </ul>
         </div>
-    </nav>
+    </div>
     """
     return nav_column
 
 def card(description: str, title: str, additional_elements:str = "") -> str:
     return f"""
-    <div class="card" style="width: 18rem;">
+    <div class="card">
         <div class="card-body">
             <h5 class="card-title">{title}</h5>
             <p class="card-text">{description}</p>
@@ -62,7 +62,7 @@ def button(style: str, text: str, alt_tag:str | None = None, additional_meta: st
         return f"""<{alt_tag} class="btn {style}" {additional_meta}>{text}</{alt_tag}>"""
 
 def bot_controls(need_token: bool) -> str:
-    ctrls = ""
+    ctrls = '<div class="row row-cols-2 row-cols-md-2 justify-content-start no-gutters">'
     if need_token:
         token_form = """
         <form action="/token" method="post">
@@ -71,19 +71,20 @@ def bot_controls(need_token: bool) -> str:
                 <input type="text" value="e.g. MTA123..." id="token" name="token" required>
             </div>
         """
-        token_form += button("btn-primary", "Save", additional_meta='type="submit"')
+        token_form += button("btn-primary", "Save", additional_meta='type="submit"') + "</div>"
         token_form += "</form>"
-        ctrls += card("We do not have a bot token saved. Please enter it here and save it.",
+        ctrls += '<div class="col">' + card("We do not have a bot token saved. Please enter it here and save it.",
                       "Save Token",
                       token_form
-                      )
+                      ) + '</div>'
     else:
-        ctrls += card("Starts the bot.",
+        ctrls += '<div class="col">' + card("Starts the bot.",
                       "Start Bot", 
-                      button("btn-primary", "Start", alt_tag="a", additional_meta='href="/start_bot"'))
-        ctrls += card("Stops the bot.",
+                      button("btn-primary", "Start", alt_tag="a", additional_meta='href="/start_bot"')) + "</div>"
+        ctrls += '<div class="col">' + card("Stops the bot.",
                       "Stop Bot",
                       button("btn-danger", "Stop", alt_tag="a", additional_meta='href="/stop_bot"')
-                      )
+                      ) + "</div>"
+    ctrls += "</div>"
     return ctrls
 
