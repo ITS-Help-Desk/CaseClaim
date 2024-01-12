2c6f3722eb009df9db31f5a8b265e3f6b18cba04
Andrew Albizati <andrew.albizati@gmail.com>
2024-01-12 13:06:40 -0800

    Replaced initilials with abb name

--- bot/cogs/heatmap_command.py
+++ bot/cogs/heatmap_command.py
@@ -59,8 +59,11 @@ class HeatmapCommand(commands.Cog):
             lead = case.lead.discord_id
             tech = case.tech.discord_id
 
-            leads[lead] = case.lead.first_name[0].upper() + case.lead.last_name[0].upper()
-            techs[tech] = case.tech.first_name[0].upper() + case.tech.last_name[0].upper()
+            #leads[lead] = case.lead.first_name[0].upper() + case.lead.last_name[0].upper()
+            #techs[tech] = case.tech.first_name[0].upper() + case.tech.last_name[0].upper()
+
+            leads[lead] = case.lead.abb_name
+            techs[tech] = case.tech.abb_name
 
             all_data.setdefault(lead, {})
             all_data[lead].setdefault(tech, 0)
